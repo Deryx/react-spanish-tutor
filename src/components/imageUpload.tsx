7a5a0012fdb3189d0077c6747173302457aba3dd
Deryx <deryxescott@gmail.com>
2023-02-14 07:03:04 -0500

    Update ImageUpload component

--- src/components/imageUpload.tsx
+++ src/components/imageUpload.tsx
@@ -1,12 +1,12 @@
 import React, { forwardRef } from 'react';
 
-const Imageupload = forwardRef( ( props:any, forwardedRef ) => {
+const Imageupload = forwardRef( ( props:any, _ref: any ) => {
     return (
         <>
             <dl>
                 <dt><label htmlFor={ props.id }>{ props.name }: </label></dt>
                 <dd>
-                    <input ref={ forwardedRef } type="file" id={ props.id } name={ props.id } />
+                    <input ref={ _ref } type="file" id={ props.id } name={ props.id } />
                 </dd>
             </dl>
         </>
