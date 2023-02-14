3486a7db18fd8b799ce348c31f3507f5608c5e01
Deryx <deryxescott@gmail.com>
2023-02-14 07:07:56 -0500

    Update TextInput component

--- src/components/textInput.tsx
+++ src/components/textInput.tsx
@@ -1,16 +1,17 @@
 import { forwardRef } from 'react';
 
-const Textinput = forwardRef( ( props:any, forwardedRef ) => {
+const Textinput = forwardRef( ( props:any, _ref: any ) => {
     return (
         <>
             <dl>
                 <dt><label htmlFor={ props.id }>{ props.name }: </label></dt>
                 <dd>
-                    <input ref={ forwardedRef } type="text" id={ props.id } onFocus={ props.onFocusEvent } name={ props.id } />
+                    <input ref={ _ref } type="text" id={ props.id } onFocus={ props.onFocusEvent } name={ props.id } />
                 </dd>
             </dl>
         </>
     )
 })
+
 
 export default Textinput;
