89912a3fe49e03ba8f3bbe818698493292fbfc87
Deryx <deryxescott@gmail.com>
2023-02-24 17:09:17 -0500

    Add pointer to site header logo hover

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cff0a84372d090fe"; }
+/******/ 		__webpack_require__.h = function() { return "37dfcc70ebe8fdba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
