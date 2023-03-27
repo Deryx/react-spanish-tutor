62ae734ab03f3e525fae0b8c933216f19d9726a7
Deryx <deryxescott@gmail.com>
2023-03-27 16:58:59 -0400

    Update site responsiveness

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7c0b56bac701904b"; }
+/******/ 		__webpack_require__.h = function() { return "877bf1e78528b925"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
